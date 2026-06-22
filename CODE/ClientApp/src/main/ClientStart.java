6cddd9e6732f0a826cca8b65fbec5637b84d27ab
Tấn Hiệp <hiepkt8018@ut.edu.vn>
2026-06-23 00:57:10 +0700

    complete function webcam

--- CODE/ClientApp/src/main/ClientStart.java
+++ CODE/ClientApp/src/main/ClientStart.java
@@ -2,7 +2,7 @@ package main;
 
 import UI.UIClient;
 
-//file này là nơi khởi động app client
+//file khởi động app client
 public class ClientStart {
     public static void main(String[] args) {
         new UIClient().run();
