a3b573c1e76c9f7ff7525fb117f2f4c7ee7d70c9
Phạm Quốc Huy <huypq0378@ut.edu.vn>
2026-06-22 13:38:56 +0700

    Update AdminServerController.java

--- CODE/AdminServer/src/AdminServerController.java
+++ CODE/AdminServer/src/AdminServerController.java
@@ -1,3 +1,4 @@
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
@@ -11,6 +12,7 @@ import java.util.List;
 import java.net.SocketException;
 
 import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 
 import config.ConfigManager;
 
@@ -24,16 +26,8 @@ public class AdminServerController {
     private List<ClientHandler> clients = new ArrayList<ClientHandler>();
     private List<Integer> freeClientNumbers = new ArrayList<Integer>();
 
-    private ServerSocket taskServerSocket;
-    private Socket taskSocket;
-
     public AdminServerController(AdminServerApp ui) {
         this.ui = ui;
-    }
-    //1
-    public Socket getTaskSocket() {
-    System.out.println("getTaskSocket = " + taskSocket);
-    return taskSocket;
     }
 
     public void startServer() {
@@ -42,34 +36,11 @@ public class AdminServerController {
             return;
         }
         serverRunning = true;
-        //2
+
         Thread serverThread = new Thread(() -> {
             try {
-                int port = ConfigManager.getInt("server_port", 1412);
-                int taskPort = 1413;
-
+                int port = ConfigManager.getInt("server_port",1412);
                 serverSocket = new ServerSocket(port);
-                taskServerSocket = new ServerSocket(taskPort);
-
-                ui.addLog("[SERVER] Screen port: " + port);
-                ui.addLog("[SERVER] Task port: " + taskPort);
-
-                // Task Manager thread
-                new Thread(() -> {
-                    try {
-                        while (serverRunning) {
-                            Socket socket = taskServerSocket.accept();
-                        
-                            taskSocket = socket;
-
-                            System.out.println("Task client connected: "
-                                    + socket.getInetAddress()
-                                 + ":" + socket.getPort());
-                        }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }).start();
 
                 String localIp = InetAddress.getLocalHost().getHostAddress();
 
@@ -111,16 +82,6 @@ public class AdminServerController {
         for (int i = 0; i < clients.size(); i++) {
             clients.get(i).close();
         }
-        public void sendPowerCommand(String clientName, String command) {
-    for (ClientHandler client : clients) {
-        if (client.clientName.equals(clientName)) {
-            client.sendMessage("POWER|" + command);
-            ui.addLog("[SERVER] Sent " + command + " command to " + clientName);
-            return;
-        }
-    }
-}
-        
 
         clients.clear();
         ui.updateOnlineCount(0);
@@ -149,6 +110,17 @@ public class AdminServerController {
             clients.get(i).sendMessage("BROADCAST|" + message);
         }
         ui.addLog("[SERVER] Sent message to " + clients.size() + " client(s): " + message);
+    }
+
+    public void sendPowerCommand(String clientName, String command) {
+        for (ClientHandler client : clients) {
+            if (client.clientName.equals(clientName)) {
+                client.sendMessage("POWER|" + command);
+                ui.addLog("[SERVER] Sent " + command + " command to " + clientName);
+                return;
+            }
+        }
+        ui.addLog("[SERVER] Client " + clientName + " not found to send power command.");
     }
 
     private class ClientHandler implements Runnable {
